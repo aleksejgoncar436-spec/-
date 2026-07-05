a058484f6ca24dbe3da2b3f64955bd887718418a
aleksejgoncar436-spec <aleksejgoncar436@gmail.com>
2026-07-05 17:53:47 +0200

    yuki_bot.py

--- yuki_bot.py
+++ yuki_bot.py
@@ -32,8 +32,9 @@ async def chat_handler(message: types.Message):
     await bot.edit_message_text(full_reply, chat_id=message.chat.id, message_id=msg.message_id)
 
 async def main():
-    print("Инициализация бота...")
-    # Убираем delete_webhook на секунду для теста авторизации
+    # Это самое мощное средство против TelegramConflictError
+    await bot.delete_webhook(drop_pending_updates=True)
+    print("Webhook удален, старые обновления сброшены.")
     await dp.start_polling(bot)
     
     print("Сессия очищена. Запуск бота...")
